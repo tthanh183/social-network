ad890ec56ff808cb04559a248a4f1dff76d59d7d
tthanh183 <trantanthanh29@gmail.com>
2024-09-17 22:14:10 +0700

    demo signup user module

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 
 import connectDB from './database/connectDB.js';
+
+import userRoutes from './routes/userRoute.js';
 
 dotenv.config();
 
@@ -9,6 +12,13 @@ connectDB();
 const app = express();
 
 const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+app.use(cookieParser());
+
+app.use('/api/users', userRoutes);
+
 
 app.listen(PORT, () => {
   console.log(`Server started at http://localhost:${PORT}`);
