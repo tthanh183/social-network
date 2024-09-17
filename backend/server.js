b7f6e0fa7e00a8e2e366204f3d233a3e862dc204
tthanh183 <trantanthanh29@gmail.com>
2024-09-17 20:48:30 +0700

    add: post && user models

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+import connectDB from './database/connectDB.js';
+
+dotenv.config();
+
+connectDB();
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server started at http://localhost:${PORT}`);
+});
